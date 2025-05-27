6b9f229f55ea5731816ed7cab75ac990233dae8d
ujwalnayak3 <115396876+ujwalnayak3@users.noreply.github.com>
2025-05-27 22:27:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/KF_requestgroup')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/KFCINEMAS')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'KF_requestgroup) # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'KF_requestgroup') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Ask_any_Movies_1')
 
 # True Or False
@@ -156,7 +156,7 @@ YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998",
 
 
 # Online Stream and Download
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://slim-dorelia-kfcinemas11-d42e4be4.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
