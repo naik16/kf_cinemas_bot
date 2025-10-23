321d83a5ec0eb373443c9de7352d4eb71ddfb0d5
ujwalnayak3 <115396876+ujwalnayak3@users.noreply.github.com>
2025-10-23 15:18:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', -1002237896833))
 
 # This Is Your Bot Support Group Id , Here Bot Will Not Give File Because This Is Support Group.
-support_chat_id = environ.get('SUPPORT_CHAT_ID', -10025240255583')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-10025240255583')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 # This Channel Is For /batch command file store.
@@ -196,4 +196,5 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
